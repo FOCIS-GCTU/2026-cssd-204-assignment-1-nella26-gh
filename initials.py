b80d58aa2327ce83c6c0d215a8d521107df38dc0
nella26-gh <chantalleamenya@gmail.com>
2026-05-30 16:20:47 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Print out my initials in bold letters
+# Assignment Number: 1
+#
+# Name: Chantalle Aku Amenya 
+# STUDENT ID:  2425402695
+# Email: chantalleamenya@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Chantalle Aku Amenya, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+#Function to print my initials in bold letters (CAA)
 
+    print("")
+    print("...CAA")
+    print("")
+
+    print("......CCCCCCCCC..........AAAAAA.............AAAAAA........")
+    print(".....CC................AAA.....AAA........AAA.....AAA.....")
+    print(".....CC................AAA.....AAA........AAA.....AAA.....")
+    print(".....CC................AAA.....AAA........AAA.....AAA.....")
+    print(".....CC................AAAAAAAAAAA........AAAAAAAAAAA.....")
+    print(".....CC................AAAAAAAAAAA........AAAAAAAAAAA.....")
+    print(".....CC................AAA.....AAA........AAA.....AAA.....")
+    print(".....CC................AAA.....AAA........AAA.....AAA.....")
+    print(".....CC...........**...AAA.....AAA...**...AAA.....AAA...**")
+    print("......CCCCCCCCC...**...AAA.....AAA...**...AAA.....AAA...**")
+
+    print("")
 
 
 main()
+
+
+    
